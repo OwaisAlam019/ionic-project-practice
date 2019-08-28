e119ab7cda046fc8a8a8e1a09421867fd253ba9a
Owais Alam <ovais.alam@sixlogs.com>
2019-08-28 18:07:35 +0500

    response name from server

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.post('/posts', function(req, res) {
 
     console.log("posts!",req.body);
 
-    res.status(200).send({hello:req.body})
+    res.status(200).send({name:req.body.name})
 });
 
 app.listen(process.env.PORT || 8080)
