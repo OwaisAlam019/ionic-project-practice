946d14c9783b6cb753f8a9d397cce0fc74865c03
OwaisAlam019 <owaisstar@gmail.com>
2019-08-28 16:33:11 +0500

    backend project setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var methodOverride = require('method-override')
+var cors = require('cors');
+
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(cors());
+
+app.get('/posts', function(req, res) {
+
+    console.log("posts!");
+
+    res.status(200).send({hello:"yoo"})
+});
+
+app.listen(process.env.PORT || 8080)
+console.log("server up..!!")
