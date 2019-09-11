3c484bbde06e4b33f71b1e06436c566ae3ad2db8
OwaisAlam019 <owaisstar@gmail.com>
2019-09-11 22:05:15 +0500

    signup page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { map } from 'rxjs/operators';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,25 +11,34 @@ import { map } from 'rxjs/operators';
 export class HomePage {
 
   name:string;
-  serverResponse  : pData;
+  // serverResponse  : pData;
   nameFromServer: string;
-  constructor(private http:HttpClient) {
+  constructor(private http:HttpClient,private toastController:ToastController) {
 
   }
-  checkName(){
-    let data = {
-      name:this.name
-    }
-    
-    this.http.post <pData> ('http://localhost:8080/posts',data).subscribe(response => {
-        console.log("POST RESPONSE",response)
-        this.nameFromServer = response.name;
-        console.log(this.serverResponse,"hello from server")
-      })
+
+  
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message:"form submitted successfully.!!",
+      duration:2000
+    })
+    toast.present()
+  }
+
+  register(form) {
+
+    this.http.post ('http://localhost:8080/posts', form.value).subscribe(response => {
+        console.log( "POST RESPONSE" , response);
+        form.reset();
+        this.presentToast();
+        // this.nameFromServer = response.name;
+        // console.log(this.serverResponse,"hello from server")
+      });
   }
 
 }
 
-export interface pData{
-  name:string
-}
+// export interface pData{
+//   result:string
+// }
